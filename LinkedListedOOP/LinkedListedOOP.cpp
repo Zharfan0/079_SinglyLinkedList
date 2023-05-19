7f2711af2d096bd343d67545f457e3de4ea80ed6
Zharfan0 <zharfanwidyadana@gmail.com>
2023-05-19 17:59:13 +0700

    case 4

--- LinkedListedOOP/LinkedListedOOP.cpp
+++ LinkedListedOOP/LinkedListedOOP.cpp
@@ -142,7 +142,46 @@ int main()
 		{
 			mhs.addNode();
 		}
+		case '2':
+		{
+			if (mhs.listEmpty())
+			{
+				cout << endl << "List Kosong" << endl;
+				break;
+			}
+			cout << endl << "\nMasukan no mahasiswa yang akan dihapus: ";
+			cin >> nim;
+			if (mhs.delNode(nim) == false)
+				cout << endl << "Data tidak ditemukan" << endl;
+			else
+				cout << endl << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus" << endl;
+		}
+		break;
+		case '3':
+		{
+			mhs.traverse();
+		}
+		break;
 
+		case '4':
+		{
+			if (mhs.listEmpty() == true)
+			{
+				cout << "\nList Kosong\n";
+				break;
+			}
+			Node* previous, * current;
+			cout << endl << "Masukan no mahasiswa yang dicari : ";
+			cin >> nim;
+			if (mhs.Search(nim, &previous, &current) == false)
+				cout << endl << "Data tidak ditemukan" << endl;
+			else
+			{
+				cout << endl << "Data ditemukan" << endl;
+				cout << "\nNo Mahasiswa: " << current->noMhs;
+				cout << "\nNama: " << current->nama;
+				cout << "\n";
+			}
 
 	}
 }
